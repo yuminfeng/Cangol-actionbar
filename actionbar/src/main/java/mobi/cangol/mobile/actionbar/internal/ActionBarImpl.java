44de389ea9cfbb83b0a3a8e18ab9d41c0ca86728
yumf <370983992@qq.com>
2024-12-10 15:21:46 +0800

    Revert "*): actionbar设置title时，判断是否展示"
    This reverts commit 24eb325b8813d2fd0fc765b8e1eeb8fe6503f47c.

--- actionbar/src/main/java/mobi/cangol/mobile/actionbar/internal/ActionBarImpl.java
+++ actionbar/src/main/java/mobi/cangol/mobile/actionbar/internal/ActionBarImpl.java
@@ -110,9 +110,6 @@ public final class ActionBarImpl extends ActionBar {
 
     @Override
     public void setTitle(int resId) {
-        if (!isShow() && resId > 0) {
-            setShow(true);
-        }
         mActionBarView.setTitle(resId);
     }
 
